fb12f256a4d026d25fb8b60fb7de909e799a356d
Fezzymon <156960669+Fezzymon@users.noreply.github.com>
2025-10-02 14:39:06 -0400

    Add files via upload

---
+++ switch-to-asusmux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo /usr/bin/supergfxctl --mode AsusMuxDgpu
+notify-send "Switched to AsusMuxDgpu mode. Reboot to apply."
---
+++ switch-to-integrated.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo /usr/bin/supergfxctl --mode Integrated
+notify-send "Switched to Integrated GPU mode. Reboot to apply."
+
